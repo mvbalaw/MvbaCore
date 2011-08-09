19ec8e3298e14ad7cbe0a637baab2377892de5e5
Clinton Sheppard <gar3ts@gmail.com>
2011-08-09 12:14:57 -0500

    add merge sort implementation

---
+++ src/MvbaCore/MergeSorter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvbaCore
+{
+	public class MergeSorter<T>
+	{
+		public IEnumerable<T> Merge(IEnumerable<T> list1, IEnumerable<T> list2, Func<T, T, int> compare)
+		{
+			var list1Pointer = list1.GetEnumerator();
+			var list2Pointer = list2.GetEnumerator();
+			bool list1HasItems = list1Pointer.MoveNext();
+			bool list2HasItems = list2Pointer.MoveNext();
+
+			while (list1HasItems || list2HasItems)
+			{
+				T list2Item;
+				if (!list1HasItems)
+				{
+					do
+					{
+						list2Item = list2Pointer.Current;
+						yield return list2Item;
+					} while (list2Pointer.MoveNext());
+					yield break;
+				}
+				T list1Item;
+				if (!list2HasItems)
+				{
+					do
+					{
+						list1Item = list1Pointer.Current;
+						yield return list1Item;
+					} while (list1Pointer.MoveNext());
+					yield break;
+				}
+
+				list2Item = list2Pointer.Current;
+				list1Item = list1Pointer.Current;
+
+				int comparisonResult = compare(list1Item, list2Item);
+				if (comparisonResult <= 0)
+				{
+					yield return list1Item;
+					list1HasItems = list1Pointer.MoveNext();
+					continue;
+				}
+
+				yield return list2Item;
+				list2HasItems = list2Pointer.MoveNext();
+			}
+		}
+	}
+}
---
+++ src/MvbaCoreTests/MergeSorterTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using FluentAssert;
+using MvbaCore;
+using NUnit.Framework;
+
+namespace MvbaCoreTests
+{
+	[TestFixture]
+	public class MergeSorterTests
+	{
+		[Test]
+		public void Given_empty_list_and_list_5_6_7_should_get_5_6_7()
+		{
+			var list1 = new int[] {};
+			var list2 = new[] {5, 6, 7};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldContainAllInOrder(new[] {5, 6, 7});
+		}
+
+		[Test]
+		public void Given_list_1_2_3_4_and_empty_list_should_get_1_2_3_4()
+		{
+			var list1 = new[] {1, 2, 3, 4};
+			var list2 = new int[] {};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldContainAllInOrder(new[] {1, 2, 3, 4});
+		}
+
+		[Test]
+		public void Given_lists_1_2_3_4_and_1_4_should_get_1_1_2_3_4_4()
+		{
+			var list1 = new[] {1, 2, 3, 4};
+			var list2 = new[] {1, 4};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldContainAllInOrder(new[] {1, 1, 2, 3, 4, 4});
+		}
+
+		[Test]
+		public void Given_lists_1_2_3_4_and_5_6_7_should_get_1_2_3_4_5_6_7()
+		{
+			var list1 = new[] {1, 2, 3, 4};
+			var list2 = new[] {5, 6, 7};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldContainAllInOrder(new[] {1, 2, 3, 4, 5, 6, 7});
+		}
+
+		[Test]
+		public void Given_lists_5_6_7_and_1_2_3_4_should_get_1_2_3_4_5_6_7()
+		{
+			var list1 = new[] {5, 6, 7};
+			var list2 = new[] {1, 2, 3, 4};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldContainAllInOrder(new[] {1, 2, 3, 4, 5, 6, 7});
+		}
+
+		[Test]
+		public void Given_two_empty_lists_should_get_an_empty_list()
+		{
+			var list1 = new int[] {};
+			var list2 = new int[] {};
+
+			var sorter = new MergeSorter<int>();
+			var result = sorter.Merge(list1, list2, (a, b) => a.CompareTo(b)).ToList();
+			result.ShouldBeEmpty();
+		}
+	}
+}

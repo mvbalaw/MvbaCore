493f71e479494db8dfdbf69642941c281f8f60b4
Weston M. Binford III <wbinford@gmail.com>
2013-05-30 13:52:12 -0500

    Updated rake file to match MVBA standard

--- src/MvbaCoreTests/Properties/AssemblyInfo.cs
+++ src/MvbaCoreTests/Properties/AssemblyInfo.cs
@@ -15,13 +15,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly:AssemblyTitle("MvbaCoreTests")]
-[assembly:AssemblyDescription("")]
-[assembly:AssemblyConfiguration("")]
-[assembly:AssemblyCompany("McCreary, Veselka, Bragg & Allen, P.C.")]
-[assembly:AssemblyProduct("MvbaCoreTests")]
-[assembly:AssemblyCopyright("Copyright © 2009 McCreary, Veselka, Bragg & Allen, P.C.")]
-[assembly:AssemblyTrademark("")]
-[assembly:AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,17 +25,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly:Guid("83597c97-918a-4d0d-84a8-7ae69d63a595")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.0.0")]

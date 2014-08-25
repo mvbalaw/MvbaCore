74b810e31c7327d80b639207e05e617ccf7656cf
Clinton <csheppard@mvbalaw.com>
2014-08-25 16:08:11 -0500

    don't call .Equals to test for null
    bugz: 37598

--- src/MvbaCore/Extensions/TExtensions.cs
+++ src/MvbaCore/Extensions/TExtensions.cs
@@ -19,13 +19,13 @@ namespace System
 		[NotNull]
 		public static T ToNonNull<T>([CanBeNull] this T input) where T : class, new()
 		{
-			return input ?? new T();
+			return ReferenceEquals(input, null) ? new T() : input;
 		}
 
 		public static TDesiredType TryCastTo<TDesiredType>(this object item) where TDesiredType : class
 		{
 			var desiredType = item as TDesiredType;
-			if (desiredType == null)
+			if (ReferenceEquals(desiredType, null))
 			{
 				throw new InvalidOperationException("Cannot convert a " + item.GetType().Name + " to a " + typeof(TDesiredType).Name + ".");
 			}
